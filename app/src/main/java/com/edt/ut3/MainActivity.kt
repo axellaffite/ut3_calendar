aed6d6deb8741fce2aa148af11cf13565fa82fd3
axel <laffite.axel81@gmail.com>
2020-07-07 18:13:02 +0200

    MainActivity nettoyé

--- app/src/main/java/com/edt/ut3/MainActivity.kt
+++ app/src/main/java/com/edt/ut3/MainActivity.kt
@@ -24,22 +24,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        RequestsManager(this).celcatService().let {
-            lifecycleScope.launchWhenCreated {
-                val body = generateCelcatBody(
-                    Date() - (6.days * 31),
-                    Date(),
-                    listOf("LINF6TDA1")
-                )
-
-                Log.d("test", body)
-
-                val events = it.getEvents(body)
-
-                println(events)
-            }
-        }
-
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
