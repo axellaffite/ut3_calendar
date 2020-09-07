cc5411d7e967d410d47321b3faad405f48469fd7
Anto <anto9375@gmail.com>
2020-09-07 23:41:17 +0200

    ✅Add notification

--- app/src/main/java/com/edt/ut3/backend/notification/NotificationManager.kt
+++ app/src/main/java/com/edt/ut3/backend/notification/NotificationManager.kt
@@ -1,9 +1,13 @@
 package com.edt.ut3.backend.notification
 
 import android.content.Context
+import android.text.format.DateFormat
+import com.edt.ut3.R
 import com.edt.ut3.backend.celcat.Event
+import fr.anto.notificationscheduler.NotificationScheduler
 
-class NotificationManager private constructor(context : Context) {
+
+class NotificationManager private constructor(val context: Context) {
     companion object {
 
         @Volatile
@@ -17,16 +21,41 @@ class NotificationManager private constructor(context : Context) {
             return INSTANCE!!
         }
     }
-    fun createNewEventsNotification(events : List<Event>)
+
+    fun createNewEventsNotification(events: List<Event>)
     {
-        //TODO Make notif apear
+        events.map {
+            NotificationScheduler.Builder(context).
+            title(context.getString(R.string.new_event_added, it.courseName)).
+            content(context.getString(R.string.new_event_added_full,android.text.format.DateFormat.format("EEE dd MMM",it.start),android.text.format.DateFormat.format("hh:mm",it.start))).
+            large_icon(R.mipmap.ic_launcher).
+            small_icon(R.mipmap.ic_launcher).
+            group("added_course").
+            build()
+        }
     }
-    fun createDeletedEventsNotification(events : List<Event>)
+    fun createDeletedEventsNotification(events: List<Event>)
     {
-        //TODO Make notif apear
+        events.map {
+            NotificationScheduler.Builder(context).
+            title(context.getString(R.string.event_deleted, it.courseName)).
+            content(context.getString(R.string.new_event_added_full,android.text.format.DateFormat.format("EEE dd MMM",it.start),android.text.format.DateFormat.format("hh:mm",it.start))).
+            large_icon(R.mipmap.ic_launcher).
+            small_icon(R.mipmap.ic_launcher).
+            group("removed_course").
+            build()
+        }
     }
-    fun createUpdatedEventsNotification(events : List<Event>)
+    fun createUpdatedEventsNotification(events: List<Event>)
     {
-        //TODO Make notif apear
+        events.map {
+            NotificationScheduler.Builder(context).
+            title(context.getString(R.string.event_updated, it.courseName)).
+            content(context.getString(R.string.event_updated_full,android.text.format.DateFormat.format("EEE dd MMM hh:mm",it.start))).
+            large_icon(R.mipmap.ic_launcher).
+            small_icon(R.mipmap.ic_launcher).
+            group("updated_course").
+            build()
+        }
     }
 }
