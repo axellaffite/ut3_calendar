0ea31d0542aac06c6a336a140762b257b09c0412
axellaffite <axel.laffite@univ-tlse3.fr>
2020-10-20 12:56:00 +0200

    doc: Documentation added to Course.kt

--- app/src/main/java/com/edt/ut3/backend/celcat/Course.kt
+++ app/src/main/java/com/edt/ut3/backend/celcat/Course.kt
@@ -4,6 +4,13 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
+/**
+ * Used to store and set the
+ * visibility on different courses.
+ *
+ * @property title The course title
+ * @property visible Whether the course is visible
+ */
 @Entity(tableName = "course")
 data class Course(
     @PrimaryKey var title : String,
