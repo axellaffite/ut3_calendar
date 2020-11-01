452a2ddc2f38aeeec56fad905b68eeeb35815f52
axellaffite <axel.laffite@univ-tlse3.fr>
2020-11-01 11:11:32 +0100

    doc: Documentation added to FragmentAuthentication.kt refactor: CompatibilityManager.kt edited, some classes such as Migration has been created. The old version is now incremented until it matches the new version code.

--- app/src/main/java/com/edt/ut3/compatibility/CompatibilityManager.kt
+++ app/src/main/java/com/edt/ut3/compatibility/CompatibilityManager.kt
@@ -8,7 +8,7 @@ import com.edt.ut3.backend.preferences.PreferencesManager
 
 object CompatibilityManager {
 
-    val PackageInfo.minorVersion: Int
+    private val PackageInfo.minorVersion: Int
         get() {
             return if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.P) {
                 versionCode
@@ -17,7 +17,7 @@ object CompatibilityManager {
             }
         }
 
-    val Context.packageInfo: PackageInfo
+    private val Context.packageInfo: PackageInfo
         get() = packageManager.getPackageInfo(packageName, 0)
 
     private lateinit var preferencesManager: PreferencesManager
@@ -25,38 +25,74 @@ object CompatibilityManager {
     fun ensureCompatibility(context: Context) {
         preferencesManager = PreferencesManager.getInstance(context)
 
-        val oldVersion = preferencesManager.codeVersion
+        var oldVersion = preferencesManager.codeVersion
         val newVersion = context.packageInfo.minorVersion
 
-        when (oldVersion to newVersion) {
-            0 to 19,
-            0 to 20 -> from0To19()
-
-            else -> {
-                Log.d(
-                    "CompatibilityManager",
-                    "Versions are the same or " +
-                            "compatibility cannot be done: " +
-                            "old=$oldVersion new=$newVersion"
-                )
-            }
+        while (oldVersion < newVersion) {
+            oldVersion = migrateFrom(Migration(oldVersion, newVersion))
         }
 
         updateVersionCode(newVersion)
     }
 
-    private fun updateVersionCode(newVersion: Int) {
-        preferencesManager.codeVersion = newVersion
+    private fun updateVersionCode(newVersion: Int) = preferencesManager.run {
+        codeVersion = newVersion
     }
 
-    private fun from0To19() {
-        preferencesManager.run {
-            if (link?.isNotEmpty() == true) {
-                link = School.default.info.first().toJSON().toString()
-            } else if (link.isNullOrBlank() && groups?.isNotEmpty() == true) {
-                link = School.default.info.first().toJSON().toString()
+    private fun migrateFrom(migration: Migration): Int = when (migration) {
+        in Migration(0,19)..Migration(0,21) -> {
+            from0To19_21()
+            21
+        }
+
+        else -> {
+            Log.d(
+                "CompatibilityManager",
+                "Versions are the same or " +
+                        "compatibility cannot be done: " +
+                        "$migration"
+            )
+
+            migration.from + 1
+        }
+    }
+
+    private fun from0To19_21(): Unit = preferencesManager.run {
+        link = School.default.info.first().toJSON().toString()
+    }
+
+    data class Migration(val from: Int, val to: Int): Comparable<Migration> {
+        operator fun rangeTo(other: Migration) = MigrationProgression(this, other)
+
+        override fun compareTo(other: Migration): Int {
+            return when (val comp = from.compareTo(other.from)) {
+                0 -> to.compareTo(other.to)
+                else -> comp
             }
         }
+    }
+
+    class MigrationIterator(
+        val startVersion: Migration,
+        val endVersion: Migration): Iterator<Migration> {
+
+        private var currentDate = startVersion
+
+        override fun hasNext() = (currentDate.to < endVersion.to)
+
+        override fun next()= currentDate.copy().also {
+            currentDate = currentDate.copy(to = currentDate.to + 1)
+        }
+    }
+
+    class MigrationProgression(
+        override val start: Migration,
+        override val endInclusive: Migration
+    ) : Iterable<Migration>,
+        ClosedRange<Migration> {
+
+        override fun iterator(): Iterator<Migration> = MigrationIterator(start, endInclusive)
+
     }
 
 }
