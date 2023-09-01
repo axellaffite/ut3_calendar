5c043daee75e5ace136098bee86c6a5ab13cac41
ElDonad <elidoset@gmail.com>
2023-09-02 00:10:40 +0200

    Update version of the work runtime
    To prevent crash linked to the missing flag in PendingIntent.broadcast

--- app/src/main/java/com/edt/ut3/backend/notification/NotificationManager.kt
+++ app/src/main/java/com/edt/ut3/backend/notification/NotificationManager.kt
@@ -279,7 +279,7 @@ class NotificationManager private constructor(val context: Context) {
             context,
             note.id.toInt(),
             notificationIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
