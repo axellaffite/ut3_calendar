ac55aa70ce9759463f83247555303647c48004f4
Elie Donadio <elidoset@gmail.com>
2023-09-02 14:57:03 +0200

    Changements liés au SDK.VERSION=33 (#30)
    * Update version of the work runtime
    
    To prevent crash linked to the missing flag in PendingIntent.broadcast
    
    * Allow notifications for SDK>=33
    
    From SDK version 33 onwards, permission to push notification is required. Added a simple check at the start of the app.

--- app/src/androidTest/java/com/edt/ut3/backend/network/events/CelcatServiceTest.kt
+++ app/src/androidTest/java/com/edt/ut3/backend/network/events/CelcatServiceTest.kt
@@ -1,5 +1,6 @@
 package com.edt.ut3.backend.network.events
 
+import com.edt.ut3.backend.background_services.updaters.ResourceType
 import com.edt.ut3.backend.requests.getClient
 import com.edt.ut3.backend.requests.celcat.CelcatService
 import com.edt.ut3.misc.extensions.minus
@@ -16,8 +17,9 @@ class CelcatServiceTest {
                 link = "https://edt.univ-tlse3.fr/calendar2",
                 start = Date().minus(Calendar.YEAR, 1),
                 formations = listOf("MINDLIHM8TPA22"),
-                emptySet(),
-                emptyMap()
+                classes = emptySet(),
+                courses = emptyMap(),
+                resType = ResourceType.Courses
             )
         }
     }
